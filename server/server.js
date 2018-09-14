402b3e9c032a5434e576aa758b1f02c9f8020152
Chels P <chelsp@Chelss-MacBook-Pro.local>
2018-09-13 23:06:47 -0400

    Hash user password before saving

--- server/server.js
+++ server/server.js
@@ -100,6 +100,7 @@ app.post('/users', (req, res) => {
     var user = new User(body);
 
     user.save().then(() => {
+        console.log(user.password);
         return user.generateAuthToken();
     }).then(token => {
         res.header('x-auth', token).send(user)
