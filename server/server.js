e17ffac41e1f6eedd552e89a198ace3be921149c
Chels P <chelsp@Chelss-MacBook-Pro.local>
2018-09-14 14:32:58 -0400

    Added DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -95,7 +95,6 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
-// POST /users 
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -111,7 +110,6 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -122,7 +120,15 @@ app.post('/users/login', (req, res) => {
     }).catch(e => {
         res.status(400).send(e);
     });
-})
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
 
 // prevent tests from "listening" twice
 if (!module.parent) {
